d0e043da30d0dba75ebc383d653fb04a1c528ab4
b-30352-del <b-30352@student.usa.edu.pk>
2026-06-03 13:21:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, render_template
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
-model = joblib.load("gaming_model.pkl")
+BASE_DIR = os.path.dirname(__file__)
+model = joblib.load(os.path.join(BASE_DIR, "gaming_model.pkl"))
+
 
 @app.route("/")
 def home():
@@ -14,24 +17,25 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
 
-    data = [
-        float(request.form["age"]),
-        float(request.form["gender"]),
-        float(request.form["gaming_hours"]),
-        float(request.form["study_hours"]),
-        float(request.form["sleep_hours"]),
-        float(request.form["attendance"]),
-        float(request.form["gaming_genre"]),
-        float(request.form["social_activity"]),
-        float(request.form["device_usage"]),
-        float(request.form["reaction_time_ms"]),
-        float(request.form["addiction_score"]),
-        float(request.form["stress_level"])
-    ]
+    data = np.array([[
+        float(request.form.get("age", 0)),
+        float(request.form.get("gender", 0)),
+        float(request.form.get("gaming_hours", 0)),
+        float(request.form.get("study_hours", 0)),
+        float(request.form.get("sleep_hours", 0)),
+        float(request.form.get("attendance", 0)),
+        float(request.form.get("gaming_genre", 0)),
+        float(request.form.get("social_activity", 0)),
+        float(request.form.get("device_usage", 0)),
+        float(request.form.get("reaction_time_ms", 0)),
+        float(request.form.get("addiction_score", 0)),
+        float(request.form.get("stress_level", 0))
+    ]])
 
-    prediction = model.predict([data])
+    prediction = model.predict(data)
 
     return f"Predicted Academic Performance: {prediction[0]}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
